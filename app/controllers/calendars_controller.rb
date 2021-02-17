91cc19b171a7ea609b1c76322e69aad60d896ea2
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-02-17 19:24:13 +0900

    イシュー4の解消

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plam).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
